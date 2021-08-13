942a75a2fadf6df9a10a9002803632afbd021088
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-13 16:59:48 -0300

    ajustes de classe active e resize

--- js/slide.js
+++ js/slide.js
@@ -1,7 +1,10 @@
+import debounce from './debounce.js'
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
+    this.activeClass = 'active'
     this.dist = {
       finalPosition: 0,
       startx: 0,
@@ -105,6 +108,7 @@ export default class Slide {
     this.moveSlide(activeSlide.position)
     this.slidesIndexNav(index)
     this.dist.finalPosition = activeSlide.position
+    this.changeActiveClass()
   }
   
   activePrevSlide() {
@@ -115,11 +119,36 @@ export default class Slide {
     if (this.index.next !== undefined) this.changeSlide(this.index.next)
   }
 
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass))
+    this.slideArray[this.index.active].element.classList.add(this.activeClass)
+  }
+  
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig()
+    this.changeSlide(this.index.active)
+    }, 1000)
+    
+  }
+  
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize)
+  }
+  
+  bindEvents() {
+    this.onStart = this.onStart.bind(this)
+    this.onMove = this.onMove.bind(this)
+    this.onEnd = this.onEnd.bind(this)
+    this.onResize = debounce(this.onResize.bind(this), 100)
+  }
+  
   init() {
     this.bindEvents()
     this.addSlideEvents()
     this.slidesConfig()
     this.transition(true)
+    this.addResizeEvent()
     return this
   }
 }
